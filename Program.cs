cabb877cf1accf53e309ee94c6ed3be0afba700a
Scott Waye <scott.waye@hubse.com>
2020-09-21 08:26:48 -0500

    1

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Console = WasmHelloWorld.Program.Console;
+
+namespace WasmHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        internal class Console
+        {
+            private static unsafe void PrintString(string s)
+            {
+                int length = s.Length;
+                fixed (char* curChar = s)
+                {
+                    for (int i = 0; i < length; i++)
+                    {
+                        TwoByteStr curCharStr = new TwoByteStr();
+                        curCharStr.first = (byte)(*(curChar + i));
+                        printf((byte*)&curCharStr, null);
+                    }
+                }
+            }
+
+            internal static void WriteLine(string s)
+            {
+                PrintString(s);
+                PrintString("\n");
+            }
+        }
+
+        struct TwoByteStr
+        {
+            public byte first;
+            public byte second;
+        }
+
+        [DllImport("*")]
+        private static unsafe extern int printf(byte* str, byte* unused);
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+Sample HelloWorld app for CoreRT
+
+- Install CoreRT as per https://github.com/dotnet/corert/blob/master/Documentation/how-to-build-and-run-ilcompiler-in-console-shell-prompt.md and https://github.com/dotnet/corert/blob/master/Documentation/how-to-build-WebAssembly.md
+
+Make sure you can run `build wasm` and it passes the tests.
+
+- Run this project with (adjust for where you have installed CoreRT) 
+```
+"E:\GitHub\corert\Tools\dotnetcli\dotnet.exe" msbuild /m /ConsoleLoggerParameters:ForceNoAlign "/p:IlcPath=E:\GitHub\corert\bin\WebAssembly.wasm.Debug" "/p:Configuration=Debug" "/p:OSGroup=WebAssembly" "/p:Platform=wasm"  "/p:FrameworkLibPath=E:\GitHub\corert\bin\WebAssembly.wasm.Debug\lib" "/p:FrameworkObjPath=E:\GitHub\corert\bin\obj\WebAssembly.wasm.Debug\Framework"  /p:NativeCodeGen=wasm WasmHelloWorld.csproj /t:LinkNative
+```
+
+- Run with 
+```
+"%EMSDK%"\node\12.18.1_64bit\bin\node bin\wasm\Debug\netcoreapp3.1\native\WasmHelloWorld.js
+```
